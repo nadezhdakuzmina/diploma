a49a8b6cfe44fe6523597898792bb2fea70070f2
Виталий Кисель <kisel@internet.ru>
2024-06-15 18:08:24 +0800

    update

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -20,5 +20,7 @@ export const BACKEND_URL = BUILD === 'client'
   ? `${scheme}://${DOMAIN}`
   : `http://${BACKEND_UPSTREAM}`;
 
+console.log('BACKEND_URL', BACKEND_URL)
+
 export const BACKEND_BASE_API_URL = `${BACKEND_URL}/api`;
 export const BACKEND_BASE_STATIC_URL = `${BACKEND_URL}/static`;
